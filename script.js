33e55de3f7f2e35caa14054bbed035c5579bd184
pequito193 <vitorspequito@gmail.com>
2022-08-18 14:18:53 +0100

    Add API call

--- script.js
+++ script.js
@@ -3,5 +3,29 @@ function slide() {
     slider.classList.toggle('slide')
 }
 
+async function lookUpData(cityname) {
+    const city = document.querySelector('.search-bar').value
+    const url = `api.openweathermap.org/data/2.5/weather?q=${cityname}&units=metric&appid=29ba00cf487b8d5735832dc033a2de17`
+    if (city.length > 0)
+    try {
+        const data = await fetch(url, {mode: 'cors'})
+        console.log(data)
+    }
+    catch (error) {
+        alert (error);
+        return null;
+    }
+}
+
 const units = document.querySelector('.units')
 units.addEventListener('click', slide)
+
+const searchBar = document.querySelector('.search-bar')
+searchBar.addEventListener('keydown', e => {
+    if (e.code === 'Enter') {
+        lookUpData();
+    }
+})
+
+const search = document.querySelector('.search-image')
+searchBar.addEventListener('click', lookUpData)
