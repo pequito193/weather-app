b0b54f8fab3602c338e31d754260803f8428573a
pequito193 <vitorspequito@gmail.com>
2022-08-19 11:23:25 +0100

    Fix API call

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let unit = 1;
+
 function slide() {
-    const slider = document.querySelector('.slider')
+    const slider = document.querySelector('.slider');
     slider.classList.toggle('slide')
+    unit ^= true;
 }
 
 const weather = (() => {
 
     // Grab the information displays
+    const message = document.querySelector('.message')
     const temperature = document.querySelector('.temperature')
     const weatherState = document.querySelector('.weather')
     const humidity = document.querySelector('.humidity')
@@ -15,7 +19,7 @@ const weather = (() => {
     // Fetch data from the API
     async function lookUpData(cityname) {
         const city = document.querySelector('.search-bar').value
-        const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=29ba00cf487b8d5735832dc033a2de17`
+        const url =`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=29ba00cf487b8d5735832dc033a2de17`
         if (city.length > 0)
         try {
             const response = await fetch(url, {mode: 'cors'});
@@ -28,12 +32,15 @@ const weather = (() => {
         }
     }
 
-    // Use the data fetched to modify the information displayed
-    // function weather() {
-    //     temperature.textContent = ${data};
-    // }
+    // Use the fetched data to display information on the webpage
+    function weatherData() {
+        let data = lookUpData();
+        message.textContent = `The current weather in ${data.city.name}, ${data.city.country} is`
+        temperature.textContent = `${data.list[0].main.temp}`;
+    }
 
-    return {lookUpData}
+
+    return {weatherData}
 })();
 
 
@@ -44,9 +51,9 @@ units.addEventListener('click', slide)
 const searchBar = document.querySelector('.search-bar')
 searchBar.addEventListener('keydown', e => {
     if (e.code === 'Enter') {
-        weather.lookUpData();
+        weather.weatherData();
     }
 })
 
 const search = document.querySelector('.search-image')
-searchBar.addEventListener('click', weather.lookUpData)
+searchBar.addEventListener('click', weather.weatherData)
