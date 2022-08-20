30e2ea590500f387da9c8015645b0f1f0a998f67
pequito193 <vitorspequito@gmail.com>
2022-08-20 15:36:49 +0100

    Finish daily forecast section

--- script.js
+++ script.js
@@ -4,24 +4,32 @@ let unit = 1;
 
 // Rounds numbers to 1 decimal point
 function round(number) {
+
+
     let result = (Math.round(number * 10) / 10).toFixed(1)
     return result;
+
+
 }
 
 
 // Function to slide the unit changer button
 function slide() {
 
+
     // Query selector the displays
     const slider = document.querySelector('.slider');
     const temperature = document.querySelector('.temperature')
     const feelsLike = document.querySelector('.feels-like-value')
 
+
     // Move the slider
     slider.classList.toggle('slide')
 
+
     // Set the unit to the opposite of the current one
     unit ^= true;
+
 
     // Change the unit on the current displays
     let string = temperature.textContent.substring(0, temperature.textContent.length - 2);
@@ -35,12 +43,39 @@ function slide() {
         feelsLike.textContent = `${round((Number(string2) - 32) / 1.8)}ºC`
     }
 
-    
+    const maxTemp = document.querySelectorAll('.max-temp')
+        let i = 0;
+        maxTemp.forEach(maxTemp => {
+            let string3 = maxTemp.textContent.substring(0, maxTemp.textContent.length - 2)
+            if (unit === 1) {
+                maxTemp.textContent = `${round((Number(string3) - 32) / 1.8)}ºC`
+            }
+            else {
+                maxTemp.textContent = `${round(Number(string3) * 1.8 + 32)}ºF`
+            }
+            i++;
+        })
+
+    const minTemp = document.querySelectorAll('.min-temp')
+        let j = 0;
+        minTemp.forEach(minTemp => {
+            let string4 = minTemp.textContent.substring(0, minTemp.textContent.length - 2)
+            if (unit === 1) {
+                minTemp.textContent = `${round((Number(string4) - 32) / 1.8)}ºC`
+            }
+            else {
+                minTemp.textContent = `${round(Number(string4) * 1.8 + 32)}ºF`
+            }
+            j++;
+        })
+
+
 }
 
 
 // Main functions that fetch and take care of the information
 const weather = (() => {
+
 
     // Grab the information displays
     const message = document.querySelector('.message')
@@ -58,6 +93,7 @@ const weather = (() => {
     const rain = document.querySelector('.rain-value')
     const date = document.querySelector('.date')
     const hour = document.querySelector('.hour')
+
 
     // Fetch data from the API
     async function lookUpData(city) {
@@ -79,6 +115,7 @@ const weather = (() => {
             pressure.textContent = `${data.list[0].main.pressure} hPa`
             population.textContent = `${data.city.population}`
 
+
             // Adjusting time values
             sunsetValue = `${new Date(data.city.sunset * 1000 + (data.city.timezone * 1000)).toUTCString()}`
             sunriseValue = `${new Date(data.city.sunrise * 1000 + (data.city.timezone * 1000)).toUTCString()}`
@@ -88,6 +125,7 @@ const weather = (() => {
             timeValue = `${new Date(+new Date() + (data.city.timezone * 1000)).toUTCString()}`
             date.textContent = timeValue.substr(0, 16)
             hour.textContent = timeValue.substr(16, 9)
+
 
             // Sets the temperature to either celsius or fahrenheit
             if (unit === 1) {
@@ -99,15 +137,16 @@ const weather = (() => {
                 feelsLike.textContent = `${round(1.8 * (data.list[0].main.feels_like - 273.15) + 32)}ºF`;
             }
 
+
             // Sets the data on the 'Daily' section
             const maxTemp = document.querySelectorAll('.max-temp')
             let i = 0;
             maxTemp.forEach(maxTemp => {
                 if (unit === 1) {
-                    maxTemp.textContent = `max ${round(data.list[i].main.temp_max - 273.15)}ºC`;
+                    maxTemp.textContent = `${round(data.list[i].main.temp_max - 273.15)}ºC`;
                 }
                 else {
-                    maxTemp.textContent = `max ${round(1.8 * (data.list[i].main.temp_max - 273.15) + 32)}ºF`;
+                    maxTemp.textContent = `${round(1.8 * (data.list[i].main.temp_max - 273.15) + 32)}ºF`;
                 }
                 i++;
             })
@@ -116,18 +155,54 @@ const weather = (() => {
             let j = 0;
             minTemp.forEach(minTemp => {
                 if (unit === 1) {
-                    minTemp.textContent = `min ${round(data.list[j].main.temp_min - 273.15)}ºC`;
+                    minTemp.textContent = `${round(data.list[j].main.temp_min - 273.15)}ºC`;
                 }
                 else {
-                    minTemp.textContent = `min ${round(1.8 * (data.list[j].main.temp_min - 273.15) + 32)}ºF`;
+                    minTemp.textContent = `${round(1.8 * (data.list[j].main.temp_min - 273.15) + 32)}ºF`;
                 }
                 j++;
             })
+
+            const weekDay = document.querySelectorAll('.week-day')
+            let k = 0;
+            weekDay.forEach(weekDay => {
+                weekDayValue = `${new Date(+new Date() + (data.city.timezone * 1000) + (86400000 * k)).toUTCString()}`
+                weekDay.textContent = weekDayValue.substr(0, 11)
+                k++
+            })
+
+            const weatherType = document.querySelectorAll('.sky')
+            let m = 0;
+            weatherType.forEach(weatherType => {
+                if (`${data.list[m].weather[0].main}` === 'Rain') {
+                    weatherType.setAttribute('src', './images/rain.png')
+                }
+                else if (`${data.list[m].weather[0].main}` === 'Clouds') {
+                    weatherType.setAttribute('src', './images/clouds.png')
+                }
+                else if (`${data.list[m].weather[0].main}` === 'Thunderstorm') {
+                    weatherType.setAttribute('src', './images/thunderstorm.png')
+                }
+                else if (`${data.list[m].weather[0].main}` === 'Snow') {
+                    weatherType.setAttribute('src', './images/snow.png')
+                }
+                else if (`${data.list[m].weather[0].main}` === 'Clear') {
+                    weatherType.setAttribute('src', './images/clear-sky.png')
+                }
+                else {
+                    weatherType.setAttribute('src', './images/idk.png')
+                }
+                m++
+            })
+
         }
+
         catch (error) {
             alert ('Place not found');
         }
+
     }
+
 
     return {lookUpData}
 })();
@@ -145,16 +220,21 @@ units.addEventListener('click', slide)
 // Add event listener to the input box
 const searchBar = document.querySelector('.search-bar')
 searchBar.addEventListener('keydown', e => {
+
     const city = document.querySelector('.search-bar').value
     if (e.keyCode === 13) {
         weather.lookUpData(city);
     }
+
+
 })
 
 
 // Add event listener to the magnifier image
 const search = document.querySelector('.search-image')
 search.addEventListener('click', e => {
+
     const city = document.querySelector('.search-bar').value
     weather.lookUpData(city);
+
 })
